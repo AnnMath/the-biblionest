0de79b5d147cf91c1efebd4f9d5a0e82cf72e20b
AnnMath <pap90@yahoo.com>
2025-04-04 11:46:56 +0200

    add book interface

--- interfaces.ts
+++ interfaces.ts
@@ -2,3 +2,16 @@ export interface Profile {
   display_name: string
   // TODO: add other profile fields if needed
 }
+
+export interface Book {
+  title: string
+  authors: string[]
+  workId: string
+  publishYear?: number
+  synopsis?: string
+  isbn?: string
+  language?: string
+  format?: string
+  pages?: number
+  coverUrl?: string
+}
