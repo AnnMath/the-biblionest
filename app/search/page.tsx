dec5b6f829a82be565eebeee256d1b780bad4175
AnnMath <pap90@yahoo.com>
2025-04-04 11:30:08 +0200

    remove unnecessary auth.ts file

---
+++ app/search/page.tsx
@@ -0,0 +1,18 @@
+const SearchPage = () => {
+  return (
+    <>
+      <article className="p-6 bg-background-300 w-full min-h-screen">
+        <h1 className="text-5xl font-bold font-heading italic text-center">
+          Explore the stacks
+        </h1>
+        <p className="my-6 max-w-lg mx-auto">
+          Search for books, discover new favourites, and add them to your
+          personal library. Whether you're building a to-be-read list or
+          revisiting old classics, start your next literary adventure here.
+        </p>
+      </article>
+    </>
+  )
+}
+
+export default SearchPage
