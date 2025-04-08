6ab9cfc661d907b99597a730ecea8c069ed8302f
AnnMath <pap90@yahoo.com>
2025-04-08 18:24:09 +0200

    fix the mask by giving an explicit size

--- components/rating/show-star-rating.tsx
+++ components/rating/show-star-rating.tsx
@@ -11,24 +11,23 @@ const ShowStarRating = ({ rating }: { rating: number | null | undefined }) => {
           maskImage: 'url(/five-stars.svg)',
           WebkitMaskRepeat: 'no-repeat',
           maskRepeat: 'no-repeat',
-          WebkitMaskSize: 'cover',
-          maskSize: 'cover',
+          WebkitMaskSize: '120px 24px',
+          maskSize: '120px 24px',
         }}
       />
-      <div className="relative w-[120px] h-6 overflow-hidden">
-        <div
-          className="absolute top-0 left-0 h-full bg-amber-400"
-          style={{
-            width: `calc(${percentage}% + 2px)`,
-            WebkitMaskImage: 'url(/five-stars.svg)',
-            maskImage: 'url(/five-stars.svg)',
-            WebkitMaskRepeat: 'no-repeat',
-            maskRepeat: 'no-repeat',
-            WebkitMaskSize: 'cover',
-            maskSize: 'cover',
-          }}
-        />
-      </div>
+
+      <div
+        className="absolute top-0 left-0 h-full bg-amber-400"
+        style={{
+          width: `calc(${percentage}% + 2px)`,
+          WebkitMaskImage: 'url(/five-stars.svg)',
+          maskImage: 'url(/five-stars.svg)',
+          WebkitMaskRepeat: 'no-repeat',
+          maskRepeat: 'no-repeat',
+          WebkitMaskSize: '120px 24px',
+          maskSize: '120px 24px',
+        }}
+      />
     </div>
   )
 }
