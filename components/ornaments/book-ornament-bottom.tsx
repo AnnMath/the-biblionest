c0c07d2c6b417b8825b85ab9bcbf431cf4e60da6
AnnMath <pap90@yahoo.com>
2025-04-09 12:49:12 +0200

    some more decoration

--- components/ornaments/book-ornament-bottom.tsx
+++ components/ornaments/book-ornament-bottom.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const BookOrnamentBottom = () => {
   return (
-    <div className="flex justify-between px-4">
+    <div className="flex justify-center gap-x-2 sm:gap-0 sm:justify-between px-4">
       <Image src="/ornament-bl.svg" alt="" height={36} width={70} />
       <Image src="/deco-bottom.svg" alt="" height={36} width={133} />
       <Image src="/ornament-br.svg" alt="" height={36} width={70} />
--- components/ornaments/book-ornament-top.tsx
+++ components/ornaments/book-ornament-top.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const BookOrnamentTop = () => {
   return (
-    <div className="flex justify-between px-4">
+    <div className="flex justify-center gap-x-2 sm:gap-0 sm:justify-between px-4">
       <Image src="/ornament-tl.svg" alt="" height={36} width={70} />
       <Image src="/deco-top.svg" alt="" height={36} width={133} />
       <Image src="/ornament-tr.svg" alt="" height={36} width={70} />
