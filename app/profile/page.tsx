f8693cac46bc8ac9d53eb00a2a41c96b4e7f085d
AnnMath <pap90@yahoo.com>
2025-04-09 23:59:06 +0200

    basic profile structure, ready for database work

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,4 @@
+import ProfileCard from '@/components/profile/profile-card'
 import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
 
@@ -27,25 +28,11 @@ const Profile = async () => {
   }
 
   const displayName = profile?.display_name || 'Reader'
+  const email = user.email
 
   return (
-    <div className="mx-auto p-4 h-screen">
-      <h1 className="text-2xl font-bold font-heading italic mb-4">
-        Hello, {displayName}!
-      </h1>
-      <div className="bg-background-200 p-6 rounded-lg shadow-md">
-        <h2 className="text-xl font-semibold mb-2">Your Profile</h2>
-        <p className="mb-4">Welcome to your personal dashboard.</p>
-
-        {/* Add more profile content here */}
-        <div className="grid gap-4 mt-6">
-          <div className="p-4 bg-background-100 rounded border border-primary-200">
-            <h3 className="font-medium text-lg">Account Information</h3>
-            <p className="text-text-600">Display Name: {displayName}</p>
-            <p className="text-text-600">Email: {user.email}</p>
-          </div>
-        </div>
-      </div>
+    <div className="bg-background-300 min-h-screen w-full py-8">
+      <ProfileCard displayName={displayName} email={email} />
     </div>
   )
 }
---
+++ components/profile/profile-card.tsx
@@ -0,0 +1,35 @@
+import BookOrnamentBottom from '../ornaments/book-ornament-bottom'
+import BookOrnamentTop from '../ornaments/book-ornament-top'
+
+const ProfileCard = ({
+  displayName,
+  email,
+}: {
+  displayName: string
+  email: string | undefined
+}) => {
+  return (
+    <div className="max-w-[1024px] w-9/10 md:w-full bg-background-50 mx-auto py-4 rounded-xl shadow-md">
+      <BookOrnamentTop />
+      <section className="grid grid-cols-1 md:grid-cols-3 p-8 justify-items-center">
+        <aside className="col-span-1 flex flex-col gap-3">
+          <h2 className="font-bold text-xl">Your profile</h2>
+          <p className="">Display Name: {displayName}</p>
+          <p className="">Email: {email}</p>
+        </aside>
+        <div className="col-span-1 md:col-span-2 gap-2 flex flex-col">
+          <h1 className="font-heading font-bold italic text-2xl">
+            Hi, {displayName}!
+          </h1>
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel tempora
+            nam architecto quisquam doloribus
+          </p>
+        </div>
+      </section>
+      <BookOrnamentBottom />
+    </div>
+  )
+}
+
+export default ProfileCard
